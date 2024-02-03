86417ba1e3f22bb5c828d074d2cbc442ed6982c1
Maria Javadyan <maria.javadyan02@gmail.com>
2024-02-03 17:46:00 +0400

    Adding private fetch function

--- js/singleton.js
+++ js/singleton.js
@@ -12,4 +12,14 @@ class Singleton {
     }
     return Singleton.instance;
   }
+  async _fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
 }
